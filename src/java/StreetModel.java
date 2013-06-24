1bc13ae92e3d04e19d5ff2924ba365445ce32bc9
Juvenal Bisneto <jove.bisneto@gmail.com>
2013-06-23 22:27:55 -0300

    Limpando arquivos que não serão usados, adicionando o mapa Galt e arrumando o funcionamento dos carros

--- src/java/StreetModel.java
+++ src/java/StreetModel.java
@@ -9,77 +9,182 @@ import jason.environment.grid.Location;
 
 public class StreetModel extends GridWorldModel {
 	public int gridSize;
-	public static final int TIMER = 1000;
+	public static final int TIMER = 300;
+	public static final int MAX_CARROS = 15;
 	
-	public static final int SEMAFORO = 8;
-	public static final int CARRO = 16;
+	public static final int RUA = 8;
+	public static final int SEMAFORO = 16;
+	public static final int CARRO = 32;
 	
-//	Location lcar1 = new Location(GridSize-1, GridSize-1);
-//	Location lcar2 = new Location(GridSize-1, GridSize-2);
-	Location origin = new Location(0, 0);
+	public Location ORIGIN;
+	public Location END;
 	
-	private HashMap<Integer, Location> inicio;
 	private HashMap<Location, Integer[]> direcoes;
 	private HashMap<Integer, Location> carros;
+	private Integer[] waitList;
 	
 	private Timer timer;
-	
-//	Area areaSem0;
-//	Area areaSem1;
 	
 	public StreetModel(int gridSize) {
 		super(gridSize,gridSize,0);
 		this.gridSize = gridSize;
 		
-		inicio = new HashMap<Integer, Location>();
 		direcoes = new HashMap<Location, Integer[]>();
 		carros = new HashMap<Integer, Location>();
+		waitList = new Integer[0];
 		
 		mapGenerator();
 		
-		addCarro(0, origin);
-		addCarro(1, new Location(2,0));
-//		for(int cont = 0; cont < 10; cont ++)
-//			moveCarros();
-
-		
 		timer = new Timer();
 		timer.schedule(new MoveTask(), TIMER);
-		
-//		Integer[] dir = new Integer[2];
-//		dir[0]=Direcao.ESQUERDA;
-//		dir[1]=Direcao.BAIXO;
-//		direcoes.put(new Location(0,0), dir);
-		
-//		addWall(1, 1, 1, 1); // obstaculo em (1,1) 
-//		setAgPos(0, GridSize-1, 0);
-//		setAgPos(1, 0, GridSize-1);
-//		add(SEMAFORO, lsem1);
-//		add(SEMAFORO, lsem2);
-		
-//		areaSem0 = new Area(new Location(0, 0), new Location(0, 1));
-//		areaSem1 = new Area(new Location(2, 1), new Location(2, 2));
-//		inicio.put(0, new Location(GridSize-1, 0));
-//		inicio.put(1, new Location(0, GridSize-1));
 	}
 	
 	
 	//MAP GENERATOR
 	public void mapGenerator(){
-		if(view != null) System.out.println("TEM VIEW");
-		Integer[] direcao = new Integer[2];
-		direcao[0] = Direcao.BAIXO;
-		direcao[1] = Direcao.DIREITA;
+		// MAPA GALT
+		ORIGIN = new Location(8,0);
+		END = new Location(0,7);
+		Integer[] esq   = new Integer[1];   esq[0] = Direcao.ESQUERDA;
+		Integer[] dir   = new Integer[1];   dir[0] = Direcao.DIREITA;
+		Integer[] cima  = new Integer[1];  cima[0] = Direcao.CIMA;
+		Integer[] baixo = new Integer[1]; baixo[0] = Direcao.BAIXO;
+		Integer[] dirBa = new Integer[2]; dirBa[0] = Direcao.DIREITA;  dirBa[1] = Direcao.BAIXO;
+		Integer[] dirCi = new Integer[2]; dirCi[0] = Direcao.DIREITA;  dirCi[1] = Direcao.CIMA;
+		Integer[] cimBa = new Integer[2]; cimBa[0] = Direcao.CIMA;  cimBa[1] = Direcao.BAIXO;
+		
+		direcoes.put(new Location(0,0), dir);
+		direcoes.put(new Location(1,0), dir);
+		direcoes.put(new Location(2,0), dir);
+		direcoes.put(new Location(3,0), baixo);
+		direcoes.put(new Location(4,0), esq);
+		direcoes.put(new Location(5,0), esq);
+		direcoes.put(new Location(6,0), esq);
+		direcoes.put(new Location(7,0), esq);
+		direcoes.put(new Location(8,0), esq);
+		
+		direcoes.put(new Location(0,1), cima);
+		direcoes.put(new Location(3,1), baixo);
+		direcoes.put(new Location(6,1), cima);
+		
+		direcoes.put(new Location(0,2), cima);
+		direcoes.put(new Location(3,2), dirBa);
+		direcoes.put(new Location(4,2), dir);
+		direcoes.put(new Location(5,2), dir);
+		direcoes.put(new Location(6,2), dirCi);
+		direcoes.put(new Location(7,2), dir);
+		direcoes.put(new Location(8,2), baixo);
+		
+		direcoes.put(new Location(0,3), cima);
+		direcoes.put(new Location(3,3), baixo);
+		direcoes.put(new Location(5,3), cima);
+		direcoes.put(new Location(8,3), baixo);
+		
+		direcoes.put(new Location(0,4), dirCi);
+		direcoes.put(new Location(1,4), dir);
+		direcoes.put(new Location(2,4), dir);
+		direcoes.put(new Location(3,4), dirBa);
+		direcoes.put(new Location(4,4), dir);
+		direcoes.put(new Location(5,4), dirCi);
+		direcoes.put(new Location(6,4), baixo);
+		direcoes.put(new Location(8,4), baixo);
+		
+		direcoes.put(new Location(0,5), cima);
+		direcoes.put(new Location(3,5), baixo);
+		direcoes.put(new Location(6,5), baixo);
+		direcoes.put(new Location(8,5), baixo);
+		
+		direcoes.put(new Location(0,6), cimBa);
+		direcoes.put(new Location(1,6), esq);
+		direcoes.put(new Location(2,6), esq);
+		direcoes.put(new Location(3,6), esq);
+		direcoes.put(new Location(4,6), esq);
+		direcoes.put(new Location(5,6), esq);
+		direcoes.put(new Location(6,6), esq);
+		direcoes.put(new Location(7,6), esq);
+		direcoes.put(new Location(8,6), esq);
+		
+		direcoes.put(new Location(0,7), baixo);
+		
+		//MAPA SIMPLES
+//		ORIGIN = new Location(5,1);
+//		END = new Location(1,5);
+//		Integer[] esq = new Integer[1];   esq[0] = Direcao.ESQUERDA;
+//		Integer[] baixo = new Integer[1]; baixo[0] = Direcao.BAIXO;
+//		Integer[] baixoEsq = new Integer[2]; baixoEsq[0] = Direcao.BAIXO; baixoEsq[1] = Direcao.ESQUERDA;
+//		direcoes.put(new Location(1,1), baixo);
+//		direcoes.put(new Location(1,2), baixo);
+//		direcoes.put(new Location(1,3), baixo);
+//		direcoes.put(new Location(1,4), baixo);
+//		direcoes.put(new Location(1,5), baixo);
+//		direcoes.put(new Location(4,2), baixo);
+//		direcoes.put(new Location(4,3), baixo);
+//		direcoes.put(new Location(2,1), esq);
+//		direcoes.put(new Location(3,1), esq);
+//		direcoes.put(new Location(5,1), esq);
+//		direcoes.put(new Location(2,4), esq);
+//		direcoes.put(new Location(3,4), esq);
+//		direcoes.put(new Location(4,4), esq);
+//		
+//		direcoes.put(new Location(4,1), baixoEsq);
+		
+		//ALL GRID
+//		for(int contX = 0; contX < gridSize; contX++){
+//			for(int contY = 0; contY < gridSize; contY++){
+//				direcoes.put(new Location(contX,contY), direcao);
+//			}
+//		}
+		
+		paintRuas();
+	}
+	private void paintRuas(){
 		for(int contX = 0; contX < gridSize; contX++){
 			for(int contY = 0; contY < gridSize; contY++){
-				direcoes.put(new Location(contX,contY), direcao);
+				Integer[] dir = direcoes.get(new Location(contX,contY));
+				if(dir != null)
+					add(RUA,new Location(contX,contY));
 			}
 		}
 	}
-	
+	public void carGenerator(){
+		if(!hasObject(CARRO,ORIGIN)){
+			if(waitList.length > 0){
+				addCarro(waitList[0], ORIGIN);
+				Integer[] aux = new Integer[waitList.length-1];
+				for(int cont = 1; cont < this.waitList.length; cont++)
+    				aux[cont-1] = new Integer(this.waitList[cont]);
+				this.waitList = aux;
+			} else if (carros.size() < MAX_CARROS){
+				addCarro(carros.size(),ORIGIN);
+			}
+		}
+	}
+	public String direcao(int x, int y){
+		String retorno = "";
+		Integer[] dir = direcoes.get(new Location(x,y));
+		for(int cont = 0; cont < dir.length; cont++){
+			switch(dir[cont]){
+			case Direcao.BAIXO:
+				retorno +=  ".";
+				break;
+			case Direcao.CIMA:
+				retorno +=  "^";
+				break;
+			case Direcao.ESQUERDA:
+				retorno +=  "<";
+				break;
+			case Direcao.DIREITA:
+				retorno +=  ">";
+				break;
+			}
+		}
+		
+		
+		return retorno;
+	}
 	
 	//CARRO
-	public boolean addCarro(int index, Location loc){
+ 	public boolean addCarro(int index, Location loc){
 		if(isFree(CARRO, loc)){
 			add(CARRO, loc);
 			carros.put(index,loc);
@@ -93,6 +198,7 @@ public class StreetModel extends GridWorldModel {
 		return true;
 	}
 	public void moveCarros(){
+		System.out.println("QTD CARROS: "+carros.size());
 		for(int cont = 0; cont < carros.size(); cont++){
 			moveCarro(cont);
 		}
@@ -119,8 +225,10 @@ public class StreetModel extends GridWorldModel {
         return true;
     }
 	public int escolheSentido(Location location){
-		System.out.println("escolhe");
 		Integer[] direcoes = this.direcoes.get(location);
+		
+		if(direcoes == null)
+			return Direcao.NADA;
 		
 		if(direcoes.length == 1){
 			return direcoes[0];
@@ -136,41 +244,28 @@ public class StreetModel extends GridWorldModel {
     	auxLocation.x += deslocamentoX;
     	auxLocation.y += deslocamentoY;
     	
-    	if(isFree(auxLocation) && inGrid(auxLocation.x, auxLocation.y)){
+    	if(!hasObject(CARRO,auxLocation) && inGrid(auxLocation.x, auxLocation.y)){
     		removeCarro(index);
-    		addCarro(index, auxLocation);
+    		if(auxLocation.x == END.x && auxLocation.y == END.y){
+    			Integer[] waitAux = new Integer[this.waitList.length+1];
+    			for(int cont = 0; cont < this.waitList.length; cont++)
+    				waitAux[cont] = new Integer(this.waitList[cont]);
+    			waitAux[waitAux.length-1] = index;
+    			this.waitList = waitAux;
+    		} else {
+    			addCarro(index, auxLocation);
+    		}
     	}
     }
-    
-    
-    //????
-    public void setPosInicial (int ag){
-		 setAgPos(ag, inicio.get(ag));
-		 
-	}
 
-    
-    //GETTERS E SETTERS
-	public HashMap<Integer, Location> getInicio() {
-		return inicio;
-	}
-	public void setInicio(HashMap<Integer, Location> inicio) {
-		this.inicio = inicio;
-	}
-	
-	public HashMap<Location, Integer[]> getDirecoes() {
-		return direcoes;
-	}
-	public void setDirecoes(HashMap<Location, Integer[]> direcoes) {
-		this.direcoes = direcoes;
-	}
     
     //TIMER TASK
 	class MoveTask extends TimerTask{
 		public void run(){
 			moveCarros();
-			timer.cancel();
+			carGenerator();
 			
+			timer.cancel();
 			timer = new Timer();
 			timer.schedule(new MoveTask(), TIMER);
 		}
--- src/java/StreetView.java
+++ src/java/StreetView.java
@@ -1,6 +1,6 @@
 import java.awt.*;
+
 import jason.environment.grid.GridWorldView;
-//import jason.environment.grid.Location;
 
 
 @SuppressWarnings("serial")
@@ -11,7 +11,7 @@ public class StreetView extends GridWorldView{
 		super(model, "Controle de Tr‰nsito", model.gridSize*60);
         smodel = model;
         model.setView(this);
-        defaultFont = new Font("Arial", Font.BOLD, 16); // change default font
+        defaultFont = new Font("Arial", Font.BOLD, 16);
         setVisible(true);
         repaint();
 	}
@@ -20,13 +20,17 @@ public class StreetView extends GridWorldView{
 	public void draw(Graphics g, int x, int y, int object) {
 		super.drawAgent(g, x, y, Color.LIGHT_GRAY, -1);
         switch (object) {
+        	case StreetModel.RUA:
+        		g.setColor(Color.DARK_GRAY);
+            	super.drawString(g, x, y, defaultFont, smodel.direcao(x,y));
+        		break;
             case StreetModel.SEMAFORO:
             	super.drawAgent(g, x, y, Color.RED, -1);
                 break;
             case StreetModel.CARRO:
-            	super.drawAgent(g, x, y, Color.BLUE, -1);
-            	g.setColor(Color.black);
-            	super.drawString(g, x, y, defaultFont, "Carro");
+            	super.drawAgent(g, x, y, Color.cyan, -1);
+            	g.setColor(Color.BLUE);
+            	super.drawString(g, x, y, defaultFont, "C");
                 break;
         }
 	}
